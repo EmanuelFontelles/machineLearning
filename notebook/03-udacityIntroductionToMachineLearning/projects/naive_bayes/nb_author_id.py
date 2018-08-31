746d20be8b270629a4c563d2b35539d8fe5436a8
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-08-31 10:53:47 -0300

    training the first model with nb and emails

--- notebook/03-udacityIntroductionToMachineLearning/projects/naive_bayes/nb_author_id.py
+++ notebook/03-udacityIntroductionToMachineLearning/projects/naive_bayes/nb_author_id.py
@@ -15,7 +15,6 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -26,8 +25,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+y_fit = clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(pred, labels_test, normalize = True)
+print(accuracy)
 #########################################################
 
 
