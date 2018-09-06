ed91ea44bda7d6258e0ed2238c0a5e46aa01c288
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-09-06 13:53:47 -0300

    added svm code in '/projects/svm'

--- notebook/03-udacityIntroductionToMachineLearning/projects/svm/svm_author_id.py
+++ notebook/03-udacityIntroductionToMachineLearning/projects/svm/svm_author_id.py
@@ -19,12 +19,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(kernel="linear")
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+#### store your predictions in a list named pred
+t0 = time()
+pred = clf.predict(features_test)
+print ("predict time:", round(time()-t0, 3), "s")
 
 #########################################################
-
-
+acc = accuracy_score(pred, labels_test, normalize = True)
+print(acc)
