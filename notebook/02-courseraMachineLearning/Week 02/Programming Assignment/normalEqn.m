40ce5e8a74c844e2666302b683cb025ce0d7c2c6
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-09-26 13:26:39 -0300

    changing folders

--- notebook/02-courseraMachineLearning/Week 02/Programming Assignment/normalEqn.m
+++
@@ -1,7 +0,0 @@
-function [theta] = normalEqn(X, y)
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
-%   regression using the normal equations.
-
-theta = pinv(X' * X) * (X' * y); % Vectorized
-
-end
