901d05a02c2046343f555a55da00547efd58d3fb
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-09-28 21:06:30 -0300

    finished decision trees

--- notebook/03-udacityIntroductionToMachineLearning/projects/decision_tree/dt_author_id.py
+++ notebook/03-udacityIntroductionToMachineLearning/projects/decision_tree/dt_author_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 """ 
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
@@ -7,25 +7,32 @@
     Sara has label 0
     Chris has label 1
 """
-    
+#%%    
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
+#%%
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+#%%
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+min_samples_split = 40
 
+clf = tree.DecisionTreeClassifier(min_samples_split=min_samples_split)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test, normalize = True)
+print(acc)
 #########################################################
 
-
+print(features_train.shape)
