53c067bc1cb683ce71837567cfd87dcca4e7f39c
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-10-23 23:17:57 -0300

    finished week 06

--- notebook/02-courseraMachineLearning/Week-06/machine-learning-ex5/linearRegCostFunction.m
+++ notebook/02-courseraMachineLearning/Week-06/machine-learning-ex5/linearRegCostFunction.m
@@ -18,20 +18,14 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h_theta = X * theta;
 
+J = 1/(2*m) * (h_theta - y)' * (h_theta - y) + (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta));
 
+thetaZero = theta;
+thetaZero(1) = 0;
 
-
-
-
-
-
-
-
-
-
+grad = ((1 / m) * (h_theta - y)' * X) + lambda / m * thetaZero';
 % =========================================================================
-
 grad = grad(:);
-
 end
