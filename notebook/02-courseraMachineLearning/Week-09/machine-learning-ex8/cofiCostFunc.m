de848b267d880688fae65cdbe20f7f10f4c85d09
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-11-12 10:21:28 -0300

    finished week 9

--- notebook/02-courseraMachineLearning/Week-09/machine-learning-ex8/cofiCostFunc.m
+++ notebook/02-courseraMachineLearning/Week-09/machine-learning-ex8/cofiCostFunc.m
@@ -39,24 +39,14 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+J = 1/2 * sum(sum((R.* ((X*Theta') - Y)).^2));
+X_grad = (R .* (X*Theta' - Y)) * Theta;
+Theta_grad = (R .* (X*Theta' - Y))' * X;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% With regularization
+J = J + lambda/2 * (sum(sum(Theta.^2)) + sum(sum(X.^2)));
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
 % =============================================================
-
 grad = [X_grad(:); Theta_grad(:)];
-
 end
