12fad023989d1a46e5391e64b145a7b747e81aaa
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-11-16 00:08:26 -0300

    finished week 10

--- notebook/03-udacityIntroductionToMachineLearning/projects/datasets_questions/utils/.ipynb_checkpoints/script-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-cat '../final_project/poi_names.txt' | awk '{print $1';', $3"' '"$2}' | sed 's/,*\r*$//' > 'new_poi_names.txt'
--- notebook/03-udacityIntroductionToMachineLearning/projects/datasets_questions/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .poi_email_addresses import poiEmails
-from .read_data import readData
-from .read_names import readNames
